4738861c8d8f574d0b602bd8228d166282841c38
mehmet çoban <ccobanmehmet@gmail.com>
2024-02-21 00:29:22 +0300

    kurulumlar yapıldı

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Link, Route  } from 'react-router-dom'
+import Home from '../src/Pages/Home/Home'
+import Profile from '../src/Pages/Profile/Profile'
+import Detail from '../src/Pages/Detail/Detail'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Test</h2>
-      <h1>test commit</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/detail' element={<Detail/>}/>
+      </Routes>
     </>
   )
 }
---
+++ myapp/src/Pages/Detail/Detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Detail() {
+  return (
+    <div>
+      <p>
+        Detail
+      </p>
+    </div>
+  )
+}
+
+export default Detail
