b03c2a554f0d038483185e90ab6f2bf7f617ee43
omerrykb <omerryuksekbag@gmail.com>
2024-03-02 23:14:42 +0300

    Add BtnNormal component and styles, update BtnOutline component
    - Add BtnNormal component with button styling and content prop
    - Update BtnOutline component to include onClick prop and add styles for hover
      effect

--- myapp/src/Pages/Home/Home.jsx
+++ myapp/src/Pages/Home/Home.jsx
@@ -8,12 +8,17 @@ import { TbHorseToy } from "react-icons/tb";
 import { GrTest } from "react-icons/gr";
 import MultiSlider from '../../assets/MultiSlider/MultiSlider';
 import BtnOutline from '../../assets/BtnOutline/BtnOutline';
+import BtnNormal from '../../assets/BtnNormal/BtnNormal';
 
 
 
 
 
 function Home() {
+
+
+  
+
   const Categories = [
     { id: 1, title: 'Trending', icon: <FaFire /> },
     { id: 3, title: 'Action', icon: <GiSpinningSword /> },
@@ -30,22 +35,22 @@ function Home() {
     { id: 15, title: 'Test', icon: <GrTest /> },
   ]
   const sliderCards2 = [
-    { years:'2012', rating: '8.2', id: 1, title: 'Card 1', content: 'Content 1', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
-    { years:'2023', rating: '7.9', id: 2, title: 'Card 2', content: 'Content 2', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
-    { years:'2013', rating: '8.9', id: 3, title: 'Card 3', content: 'Content 3', img: 'https://m.media-amazon.com/images/M/MV5BMjE0MjIwMDE2MV5BMl5BanBnXkFtZTgwMzM5MDQzNTM@._V1_.jpg' },
-    { years:'2002', rating: '7.5', id: 4, title: 'Card 4', content: 'Content 4', img: 'https://m.media-amazon.com/images/I/51fWOBx3agL._AC_.jpg' },
-    { years:'2008', rating: '6.9', id: 5, title: 'Card 5', content: 'Content 5', img: 'https://i.ebayimg.com/images/g/R-0AAOSw-jhUEyJ1/s-l400.jpg' },
-    { years:'2015', rating: '6.9', id: 6, title: 'Card 6', content: 'Content 6', img: 'https://i.ebayimg.com/images/g/onAAAOSw~W5ifP7J/s-l1200.jpg' },
-    { years:'2022', rating: '8.2', id: 7, title: 'Card 7', content: 'Content 7', img: 'https://i.pinimg.com/474x/38/b7/55/38b75579600e6d4c098ed2a0243aec52.jpg' },
-    { years:'2013', rating: '8.9', id: 8, title: 'Card 8', content: 'Content 8', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
-    { years:'2009', rating: '9.9', id: 9, title: 'Card 9', content: 'Content 9', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
-    { years:'2011', rating: '8.9', id: 10, title: 'Card 10', content: 'Content 10', img: 'https://m.media-amazon.com/images/I/51fWOBx3agL._AC_.jpg' },
-    { years:'2005', rating: '8.2', id: 11, title: 'Card 11', content: 'Content 11', img: 'https://i.ebayimg.com/images/g/R-0AAOSw-jhUEyJ1/s-l400.jpg' },
-    { years:'2019', rating: '8.9', id: 12, title: 'Card 12', content: 'Content 12', img: 'https://i.ebayimg.com/images/g/onAAAOSw~W5ifP7J/s-l1200.jpg' },
-    { years:'2020', rating: '7.9', id: 13, title: 'Card 13', content: 'Content 13', img: 'https://i.pinimg.com/474x/38/b7/55/38b75579600e6d4c098ed2a0243aec52.jpg' },
-    { years:'2021', rating: '8.9', id: 14, title: 'Card 14', content: 'Content 14', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
-    { years:'2022', rating: '8.2', id: 15, title: 'Card 15', content: 'Content 15', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
-    { years:'2023', rating: '6.5', id: 16, title: 'Card 16', content: 'Content 16', img: 'https://m.media-amazon.com/images/M/MV5BMjE0MjIwMDE2MV5BMl5BanBnXkFtZTgwMzM5MDQzNTM@._V1_.jpg' },
+    { years: '2012', rating: '8.2', id: 1, title: 'Card 1', content: 'Content 1', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
+    { years: '2023', rating: '7.9', id: 2, title: 'Card 2', content: 'Content 2', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
+    { years: '2013', rating: '8.9', id: 3, title: 'Card 3', content: 'Content 3', img: 'https://m.media-amazon.com/images/M/MV5BMjE0MjIwMDE2MV5BMl5BanBnXkFtZTgwMzM5MDQzNTM@._V1_.jpg' },
+    { years: '2002', rating: '7.5', id: 4, title: 'Card 4', content: 'Content 4', img: 'https://m.media-amazon.com/images/I/51fWOBx3agL._AC_.jpg' },
+    { years: '2008', rating: '6.9', id: 5, title: 'Card 5', content: 'Content 5', img: 'https://i.ebayimg.com/images/g/R-0AAOSw-jhUEyJ1/s-l400.jpg' },
+    { years: '2015', rating: '6.9', id: 6, title: 'Card 6', content: 'Content 6', img: 'https://i.ebayimg.com/images/g/onAAAOSw~W5ifP7J/s-l1200.jpg' },
+    { years: '2022', rating: '8.2', id: 7, title: 'Card 7', content: 'Content 7', img: 'https://i.pinimg.com/474x/38/b7/55/38b75579600e6d4c098ed2a0243aec52.jpg' },
+    { years: '2013', rating: '8.9', id: 8, title: 'Card 8', content: 'Content 8', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
+    { years: '2009', rating: '9.9', id: 9, title: 'Card 9', content: 'Content 9', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
+    { years: '2011', rating: '8.9', id: 10, title: 'Card 10', content: 'Content 10', img: 'https://m.media-amazon.com/images/I/51fWOBx3agL._AC_.jpg' },
+    { years: '2005', rating: '8.2', id: 11, title: 'Card 11', content: 'Content 11', img: 'https://i.ebayimg.com/images/g/R-0AAOSw-jhUEyJ1/s-l400.jpg' },
+    { years: '2019', rating: '8.9', id: 12, title: 'Card 12', content: 'Content 12', img: 'https://i.ebayimg.com/images/g/onAAAOSw~W5ifP7J/s-l1200.jpg' },
+    { years: '2020', rating: '7.9', id: 13, title: 'Card 13', content: 'Content 13', img: 'https://i.pinimg.com/474x/38/b7/55/38b75579600e6d4c098ed2a0243aec52.jpg' },
+    { years: '2021', rating: '8.9', id: 14, title: 'Card 14', content: 'Content 14', img: 'https://i.pinimg.com/474x/75/7f/26/757f26a9d80eae652757df2dacd6aef6.jpg' },
+    { years: '2022', rating: '8.2', id: 15, title: 'Card 15', content: 'Content 15', img: 'https://tr.web.img3.acsta.net/r_1280_720/medias/nmedia/18/85/59/68/19816803.jpg' },
+    { years: '2023', rating: '6.5', id: 16, title: 'Card 16', content: 'Content 16', img: 'https://m.media-amazon.com/images/M/MV5BMjE0MjIwMDE2MV5BMl5BanBnXkFtZTgwMzM5MDQzNTM@._V1_.jpg' },
   ];
 
 
@@ -74,7 +79,8 @@ function Home() {
                     <div className="mt-4 child-5 d-flex gap-3">
                       {/* <button className='w-25 p-2 rounded-5 m-0 text-white fw-semibold'>More</button>
                       <button className='w-25 p-2 rounded-5 m-0 text-white fw-semibold'>Lorem</button> */}
-                      <BtnOutline />
+                      <BtnOutline Content={'LOREM'} />
+                      <BtnNormal Content={'MORE'} />
                     </div>
                   </div>
                 </Col>
@@ -104,7 +110,7 @@ function Home() {
       </div>
       {/* filter div end */}
 
-      
+
       <div className="multi-slider-bg ">
         <Container fluid='md'>
           <MultiSlider Data={sliderCards2} />
--- myapp/src/assets/BtnOutline/BtnOutline.jsx
+++ myapp/src/assets/BtnOutline/BtnOutline.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import './BtnOutline.scss'
 
-function BtnOutline() {
-  return (
-    <>
-    
-    </>
-  )
+function BtnOutline({ Content, onClick }) {
+    return (
+        <div className='BtnOutline'>
+            <button className='MyBtn py-2 px-5 rounded-4 fw-bold text-center' onClick={onClick}>
+                {Content}
+            </button>
+        </div>
+    )
 }
 
 export default BtnOutline
